26ef9b718d18d9bd76d804fbe34650d2e8755891
prashantsingh2408 <prashantsingh2408@gmail.com>
2024-12-23 17:16:22 +0530

    update web build

--- snake/build/web/flutter_bootstrap.js
+++ snake/build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2223377215"
+    serviceWorkerVersion: "4281686928"
   }
 });
